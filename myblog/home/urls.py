71ce7fa9e3f607331e0767e1ad36d8bcfd7abd35
Mazhar Rasheed <mazharopal@gmail.com>
2024-03-17 14:08:38 +0500

    many chages

--- myblog/home/urls.py
+++ myblog/home/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
    
     path('', views.index,name="index"),
     path('dashboard/', views.dashboard,name="dashboard"),
+    path('postblog/', views.post_blog,name="postblog"),
     path('signup/', views.sign_up,name="signup"),
     path('signin/', views.sign_in,name="signin"),
     path('logout/', views.log_out,name="logout"),
+    path('detail/<int:id>', views.detail,name="detail"),
+    path('delete/<int:id>', views.delete_data , name="deletedata"),
+    path('edit/<int:id>', views.edit_data , name="editdata"),
+    path('editprofile/<int:id>', views.editprofile , name="editprofile"),
 ]
--- myblog/home/views.py
+++ myblog/home/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import (authenticate, login, logout,
                                  update_session_auth_hash)
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
@@ -11,24 +12,63 @@ from .models import Blog
 # Create your views here.
 
 def index(request):
-  if request.user.is_authenticated:
-    myblog=Blog.objects.all()
-  else:
-    return redirect('signin')
+  # if request.user.is_authenticated:
+  myblog=Blog.objects.all()
+  # else:
+    # return redirect('signin')
   data={'myblog':myblog}
   return render(request,'index.html',data)
 
+def detail(request,id):
+
+  myblog=Blog.objects.get(id=id)
+  data={'myblog':myblog}
+  return render(request,'detail.html',data)
+
+def edit_data(request,id): 
+    data={}
+    if request.method=='POST':
+        pst=Blog.objects.get(id=id)
+        form=Add_Blog(request.POST,instance=pst)
+        if form.is_valid():
+            form.save()
+    else:
+        pst=Blog.objects.get(id=id)
+        form=Add_Blog(instance=pst)
+    data={'form':form,'pst':pst}
+    return render(request,'update.html',data)
+
+def delete_data(request,id):
+  if request.user.is_authenticated:
+    user=request.user
+    perm=User.get_user_permissions(user)
+    for pm in perm:
+      if pm=="home.delete_blog":
+        pst=Blog.objects.get(id=id)
+        pst.delete()
+      else:
+        pass
+    return redirect('dashboard')
+  else:
+    return redirect('signin')
+
+def post_blog(request):
+  if request.user.is_authenticated:
+    if request.method=='POST':
+      form=Add_Blog(request.POST)
+      form.save()
+      return redirect('dashboard')
+    else:
+      form=Add_Blog()
+    data={'form':form}
+    return render(request,'postblog.html',data)
+  else:
+    return redirect('signin')
 
 def dashboard(request):
   if request.user.is_authenticated:
     myblog=Blog.objects.all()
-
-    if request.method=='POST':
-      form=Add_Blog(request.POST)
-      form.save()
-    else:
-      form=Add_Blog()
-    data={'myblog':myblog,'form':form}
+    data={'myblog':myblog}
     return render(request,'dashboard.html',data)
   else:
     return redirect('signin')
@@ -63,7 +103,7 @@ def sign_in(request):
         user = authenticate(username=uname, password=upass)
         if user is not None:
           login(request, user)
-          messages.success(request, "You are successfuly login")
+          messages.success(request, "You are successfuly Signin")
           return HttpResponseRedirect("/dashboard/")
     else:
       login_form = AuthenticationForm()
@@ -73,4 +113,12 @@ def sign_in(request):
   else:
     return redirect("dashboard")
 
+def editprofile(request,id):
+  if request.user.is_authenticated:
+
+
+    mydata = {}
+    return render(request,"editprofile.html", mydata)
+  else:
+    return redirect('signin')
   
